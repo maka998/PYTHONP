c8f91751adcb0a2b2c0f242859a55ec8dd53614d
marija.vucetic <marijavucetic@uns.ac.rs>
2020-02-19 23:24:59 +0100

    main

---
+++ app.py
@@ -0,0 +1,40 @@
+#import tasks
+#import repositoryUtils as ru
+
+
+def pickOption(option):
+    switcher = {
+      #  0: tasks.exit,
+      #  1: tasks.directorySelection,
+      #  3: tasks.setPaginationN,
+      #  4: tasks.wordSearch,
+      #  #  5: tasks.printContent,
+    }
+    func = switcher.get(option, lambda: "Invalid option")
+    func()
+    # Ako smo odabrali izlaz, nasilno izadji i ne pitaj nista
+    if option != 0:
+        quitOpt = input(" .. press any key to continue ... ")
+
+
+n = -1  # predefinisana peginacija
+
+
+def main():
+    option = -1
+    while option != 0:
+        try:
+          #  ru.cls()  # brisanje ekrana
+            print("[1] Odabir direktorijuma")
+            print("[3] Unesi n za paginaciju")
+            print("[4] Pretraga reci ")
+            print("[5] Ispisis sadrzaj")
+            print("[0] Prekid")
+
+            option = int(input())
+            pickOption(option)
+        except ValueError:
+            print("Please enter an integer")
+
+
+main()
